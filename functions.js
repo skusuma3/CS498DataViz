e679caa28e3a8538c4434c108fdc7b37802fc7d2
skusuma3 <31798987+skusuma3@users.noreply.github.com>
2018-07-15 16:34:40 -0700

    bar

--- functions.js
+++ functions.js
@@ -89,8 +89,8 @@ function loadCSV1(){
         console.log("SectorsAggregate:" + sectorAggregate.length);
 
         sectorH = sortHashTable(sectorAggregate);
-        console.log(sectorH);
-        console.log(sectorH[0][0]);
+        //console.log(sectorH);
+        //console.log(sectorH[0][0]);
         console.log("Dataset loading complete.");
         //prepareSectorAggregates();
         updateViz(sectorH)
@@ -133,11 +133,16 @@ function updateViz(data)
   document.getElementById("loader").style.display = "none";
   
   // There are enought html elements and here following Update Pattern
-  d3.select("#list").selectAll("li").data(sectorH)
+  d3.select("#list").selectAll("li").data(sectorAggregate)
     .text(function(d) { return d[0]; })
     .enter().append("li")
     .text(function(d)  {return d[0] + ":"+ d[1];})
  
+    d3.select("#listsorted").selectAll("li").data(sectorH)
+    .text(function(d,i) { return d[i][0]; })
+    .enter().append("li")
+    .text(function(d,i)  {return d[i][0] + ":"+ d[i][1];})
+
     //d3.select("p")
     //    .datum(50)
     //    .text(function(d) { 
@@ -154,51 +159,14 @@ function updateViz(data)
             return d;
             });
 
-    var svg = d3.select("svg"),
-        margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom;
-        
-        var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-            y = d3.scaleLinear().rangeRound([height, 0]);
-        
-        var g = svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        
-        d3.csv("data/kiva_loans-cols.csv", function(d) {
-            d.funded_amount =+d.funded_amount;
-            return d;
-        }, function(error, data) {
-            if(error) throw error;
+    var xScale = d3.scale.ordinal()
+        .domain(d3.range(sectorH.length))
+        .rangeRoundBands([0, w], 0.05); 
 
-            x.domain(data.map(function(d) { return d.sector; }));
-            y.domain([0, d3.max(data, function(d) { return d.funded_amount; })]);
-          
-            g.append("g")
-                .attr("class", "axis axis--x")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x));
-          
-            g.append("g")
-                .attr("class", "axis axis--y")
-                .call(d3.axisLeft(y).ticks(10, "%"))
-              .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", "0.71em")
-                .attr("text-anchor", "end")
-                .text("Frequency");
-          
-            g.selectAll(".bar")
-              .data(data)
-              .enter().append("rect")
-                .attr("class", "bar")
-                .attr("x", function(d) { return x(d.sector); })
-                .attr("y", function(d) { return y(d.funded_amount); })
-                .attr("width", x.bandwidth())
-                .attr("height", function(d) { return height - y(d.funded_amount); });
-          });
-            
+    var yScale = d3.scale.linear()
+        .domain([0, d3.max(sectorH.value)])
+        .range([0, h]);
+
   // Each rect element should have the following properties
   // set as attributes:
   // "width" = 15
