6f8c29fff2d974e4cc2435c5b33e04ff3c14b1c1
skusuma3 <31798987+skusuma3@users.noreply.github.com>
2018-07-15 17:54:42 -0700

    bar

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ var Loans = [];
 //var SectorLoans =[];
 //var SectorLoans = [ ["Food", 300], ["Transportation", 575],["Arts", 200],["Food", 400],["Services", 250]]
 var sectorAggregate = [];
-var sectorAggregate1 = [];
+var sectorAggregate1 = {};
 var sectorH = [];
 
 function kivaLoanEntry(id, funded_amount, sector, country, partner_id, loandate ) {
@@ -17,13 +17,15 @@ function kivaLoanEntry(id, funded_amount, sector, country, partner_id, loandate 
     this.loandate = loandate;
   };
  
+  var sec = [];
+  var secValues = []
+
   function AddLoan(row) {
     Loans.push(new kivaLoanEntry(+row.id, +row.funded_amount, row.sector, row.country, row.partner_id, row.date));
     //SectorLoans.push( [row.sector, +row.funded_amount] );
 
     if (!sectorAggregate1.hasOwnProperty(row.sector)) {
-        sectorAggregate1.push( { key: row.sector, value: +row.funded_amount } );
-        //sectorAggregate[row.sector] = +row.funded_amount;
+        sectorAggregate1.push( row.sector, +row.funded_amount );
     } else {
         sectorAggregate1[row.sector] += +row.funded_amount;
     }
